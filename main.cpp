a46aa14f85ab90ef04325334584075c4a7703a68
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-09-14 00:20:18 +0530

    ADDED Code to convert into model objects

--- main.cpp
+++ main.cpp
@@ -6,6 +6,58 @@
 
 using namespace std;
 
+class Batsman
+{
+    //POS,PLAYER,     Mat,Inns,NO,Runs,HS,  Avg, BF, SR,    100,50,4s,6s
+    //1,David Warner ,12, 12,  2, 692, 100*,69.2,481,143.86, 1,  8,57,21
+
+    string name;
+    double avg = 0;
+    double stk_rate = 0;
+    int sixes = 0;
+    int fours = 0;
+    int runs = 0;
+
+public:
+    Batsman(string name)
+    {
+        this->name = name;
+    }
+
+    Batsman(vector<string> obj_params)
+    {
+        if (obj_params.size() >= 14)
+        {
+            this->name = obj_params.at(1);
+            this->avg = stod(obj_params.at(7));
+            this->stk_rate = stod(obj_params.at(9));
+            this->sixes = stoi(obj_params.at(13));
+            this->fours = stoi(obj_params.at(12));
+            this->runs = stoi(obj_params.at(5));
+        }
+        else
+            cout << "Length matching error !!!";
+    }
+
+    void set_runs_4s_6s(int runs, int fours, int sixes)
+    {
+        this->runs = runs;
+        this->fours = fours;
+        this->sixes = sixes;
+    }
+
+    void set_avg_and_strike(double stk_rate, double avg)
+    {
+        this->avg = avg;
+        this->stk_rate = stk_rate;
+    }
+
+    string to_string()
+    {
+        return this->name + " " + std::to_string(this->avg) + "\n";
+    }
+};
+
 class CSV_Reader
 {
 public:
@@ -39,11 +91,27 @@ int main(int argc, char const *argv[])
 {
     CSV_Reader csv_reader;
     list<vector<string>> demo = csv_reader.readFromFile("./Resources/MostRuns.csv");
+    list<Batsman> batting;
     for (vector<string> temp : demo)
     {
-        for (string ss : temp)
-            cout << ss << " ";
+        cout << temp.at(1) << " ";
+        cout << temp.at(7) << " ";
+        cout << temp.at(9) << " ";
+        cout << temp.at(13) << " ";
+        cout << temp.at(12) << " ";
+        cout << temp.at(5) << " ";
+
         cout << endl;
     }
+
+    for (vector<string> temp : demo)
+    {
+        Batsman *batsman = new Batsman(temp);
+        batting.push_back(*batsman);
+    }
+
+    for (Batsman batsman : batting)
+        cout << batsman.to_string();
+
     return 0;
 }
